d9f670d82ab6785363fea96942bbc7401636dcf8
Djumabaevs <djumabaevb@gmail.com>
2021-06-16 09:56:54 +0600

    findNavController().navigate

--- app/src/main/java/com/example/fragmentnavigationcompose/RecipeListFragment.kt
+++ app/src/main/java/com/example/fragmentnavigationcompose/RecipeListFragment.kt
@@ -9,15 +9,21 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Text
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import com.example.fragmentnavigationcompose.ui.theme.HorizontalDottedProgress
 
 class RecipeListFragment : Fragment() {
@@ -26,7 +32,35 @@ class RecipeListFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val view = inflater.inflate(R.layout.fragment_recipe_list, container, false)
+
+        return ComposeView(requireContext()).apply {
+            setContent {
+                Column(modifier = Modifier.padding(16.dp)) {
+                    Text(
+                        text = "Recipe List",
+                        fontSize = 21.sp
+                    )
+                    Spacer(modifier = Modifier.padding(10.dp))
+                    Button(onClick = {
+                        findNavController().navigate(R.id.viewRecipe)
+                    }
+                    ) {
+                        Text(text = "To Recipe Fragment")
+
+                    }
+                    
+                }
+            }
+        }
+
+
+
+
+
+
+
+
+       /* val view = inflater.inflate(R.layout.fragment_recipe_list, container, false)
         view.findViewById<ComposeView>(R.id.compose_view).setContent {
             Column(modifier = Modifier
                 .border(border = BorderStroke(1.dp, Color.Black))
@@ -43,7 +77,12 @@ class RecipeListFragment : Fragment() {
                 val customView = HorizontalDottedProgress(LocalContext.current)
                 AndroidView(factory = {customView})
             }
-        }
+        }*/
+
+
+
+
+
 
     /*    val view = ComposeView(requireContext())
         view.apply {
@@ -51,6 +90,6 @@ class RecipeListFragment : Fragment() {
                 Text(text = "Recipe List Composable")
             }
         }*/
-        return view
+
     }
 }
